63fd7b342b5381e4a215cb583f3008791f9b0297
Besher <besher333@outlook.sa>
2021-10-24 20:46:15 +0300

    add prewitt kernel

--- kernel.c
+++ kernel.c
@@ -17,6 +17,7 @@
 #define MIN_KERNEL_SIZE 3
 #define LINE_SIZE 100
 #define PADDING_VALUE 0
+#define MAGIC_NUMBER_SIZE 3
 #define MAX_PIXEL_VAL 255
 #define BRAND_NAME "Besher"
 
@@ -24,6 +25,7 @@
 void setColor(int);
 void removeExtension(char*, char*);
 bool isSpace(char);
+int* staticToDynamicKernel(int[], int);
 
 //generic PGM functions
 bool isBinaryPgm(FILE*);
@@ -35,35 +37,89 @@ bool writeArrToPgm(uint8_t*, int, int, char*, int);
 uint8_t* allocArrWithPad(int, int, int);
 size_t fwritePixels(const void*, size_t, size_t, FILE*);
 uint8_t* rBinaryPgmPad(char*, int*, int*, int);
+uint8_t* rAsciiPgmPad(char*, int*, int*, int);
 int* applyKernelArr(int*, int, float, uint8_t*, int, int);
-int applyKernelPix(int*, int, float, uint8_t*, int, size_t, size_t);
-uint8_t* filterScale(int*, int, int);
+int applyKernelPixWithCo(int*, int, float, uint8_t*, int, size_t, size_t);
+int applyKernelPixNoCo(int*, int, float, uint8_t*, int, size_t, size_t);
+uint8_t* filterSlice(int*, int, int);
 uint8_t* filterMinMax(int*, int, int);
+void setPaddingMirror(uint8_t*, int, int, int);
+uint8_t* addStaticPad(uint8_t*, uint8_t, int, int, int);
+bool doesKernelFit(int, int, int);
+
+//kernel-specific functions
+uint8_t* applyPrewittVertical(uint8_t*, int, int, bool);
+
 
 int main(int argc, char const *argv[]) {
-  uint8_t* arr, *pixelValues;
+  uint8_t *arr, *pixelValues;
   int width, height, *arrValues;
-  int kernel[9] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-  arr = rBinaryPgmPad(argv[1], &width, &height, 3);
-  arrValues = applyKernelArr(kernel, 3, 1, arr, width+2, height+2);
+  int kernelSize = 3;
+  arr = rBinaryPgmPad(argv[1], &width, &height, 0);
+  if (!arr)
+    arr = rAsciiPgmPad(argv[1], &width, &height, 0);
+  if (!arr)
+    return 1;
+  pixelValues = applyPrewittVertical(arr, width, height, true);
   free(arr);
-  pixelValues = filterMinMax(arrValues, width, height);
-  free(arrValues);
   writeArrToPgm(pixelValues, width, height, argv[2], 2);
   free(pixelValues);
   return 0;
 }
 
+/*
+* Function: applyPrewittVertical
+* --------------------------
+* returns a pointer to an array with vertical edge detection filter applied
+*
+* pixelValues: pointer to the array representing image that will be processed
+* width: image width without padding
+* height: image height without padding
+* postPadding: a bool value indicating whether the padding will be applied
+* before (false) or after (true) kernel
+*
+* returns: a pointer to the newly allocated array after processing
+*/
+uint8_t* applyPrewittVertical(uint8_t* pixelValues, int width, int height, bool postPadding) {
+  int staticKernel[3*3] = {1, 1, 1, 0, 0, 0, -1, -1, -1};
+  int kernelSize = 3, *kernel, padding, *filteredValues;
+  uint8_t *pixelValuesCopy, *filteredPixels;
+  if (!doesKernelFit(kernelSize, width, height) && !postPadding) {
+    setColor(RED);
+    printf("Kernel too be big to be applied without pre-padding\n");
+    setColor(RESET);
+    return NULL;
+  }
+  kernel = staticToDynamicKernel(staticKernel, kernelSize);
+  pixelValuesCopy = (uint8_t*) malloc(width * height * sizeof(uint8_t));
+  memcpy(pixelValuesCopy, pixelValues, width*height * sizeof(uint8_t));
+  if (!postPadding) {
+    pixelValuesCopy = addStaticPad(pixelValuesCopy, 0, width, height, kernelSize);
+    padding = (kernelSize>>1);
+  } else {
+    padding = 0;
+  }
+  filteredValues = applyKernelArr(kernel, kernelSize, 1, pixelValuesCopy, width+padding*2, height+padding*2);
+  free(pixelValuesCopy);
+  padding = (kernelSize>>1);
+  filteredPixels = filterSlice(filteredValues, width-padding*2, height-padding*2);
+  free(filteredValues);
+  if (postPadding) {
+    filteredPixels = addStaticPad(filteredPixels, 0, width-padding*2, height-padding*2, kernelSize);
+  }
+  free(kernel);
+  return filteredPixels;
+}
+
 uint8_t* rBinaryPgmPad(char* fileName, int* width, int* height, int kernelSize) {
   FILE* picture;
   uint8_t* pixelValues;
-  size_t i, j;
   int numberHolder;
-  int padding = kernelSize>>1;
   if (kernelSize < MIN_KERNEL_SIZE && kernelSize % 2) {
     setColor(RED);
     printf("Error: kernel size must be >= %d and odd", MIN_KERNEL_SIZE);
     setColor(RESET);
+    return NULL;
   }
   if (!(picture = fopen(fileName, "rb"))) {
     setColor(RED);
@@ -79,43 +135,137 @@ uint8_t* rBinaryPgmPad(char* fileName, int* width, int* height, int kernelSize) 
     return NULL;
   }
   skipComments(picture);
-  char* lineHolder = (char*) malloc(LINE_SIZE * sizeof(char));
   fscanf(picture, "%d %d", width, height);
   // get maximum value
   skipComments(picture);
   fscanf(picture, "%d", &numberHolder);
   fgetc(picture);
-  free(lineHolder);
   // rowWidth is the width plus the padding on both sides
-  int rowWidth = *width + padding*2;
-  pixelValues = allocArrWithPad(*width, *height, kernelSize);
+  pixelValues = (uint8_t*) malloc((*height) * (*width) * sizeof(uint8_t));
   if (!pixelValues) {
     setColor(RED);
     printf("%s:%d > Failed to allocate memory\n", __FILE__, __LINE__);
     setColor(RESET);
     return NULL;
   }
-  //initialize padding rows with 0
-  for (j=0, i=0; j < padding; i++, j++)
-    memset(pixelValues + i*rowWidth, PADDING_VALUE, rowWidth);
-  //set pixelvalues adding padding at the beginning and end of each row
-  for (j=0; j < *height; i++, j++) {
-    memset(pixelValues + i*rowWidth, PADDING_VALUE, padding);
-    fread(pixelValues + i*rowWidth + padding, sizeof(uint8_t), (*width), picture);
-    memset(pixelValues + i*rowWidth + padding + *width, PADDING_VALUE, padding);
-  }
-  //initialize last padding rows with 0
-  for (j=0; j < padding; i++,j++)
-    memset(pixelValues + i*rowWidth, PADDING_VALUE, rowWidth);
+  fread(pixelValues, sizeof(uint8_t), (*width) * (*height), picture);
   fclose(picture);
-  return pixelValues;
+  if (kernelSize) {
+    return addStaticPad(pixelValues, 0, *width, *height, kernelSize);
+  } else {
+    return pixelValues;
+  }
+}
+
+uint8_t* rAsciiPgmPad(char* fileName, int* width, int* height, int kernelSize) {
+  FILE* picture;
+  uint8_t* pixelValues;
+  size_t i = 0;
+  int numberHolder;
+  if (kernelSize < MIN_KERNEL_SIZE && kernelSize % 2) {
+    setColor(RED);
+    printf("Error: kernel size must be >= %d and odd", MIN_KERNEL_SIZE);
+    setColor(RESET);
+    return NULL;
+  }
+  if (!(picture = fopen(fileName, "rb"))) {
+    setColor(RED);
+    printf("Couldn't read %s\nProbably doesn't exist\n", fileName);
+    setColor(RESET);
+    return NULL;
+  }
+  if (!isAsciiPgm(picture)) {
+    setColor(RED);
+    printf("%s is not an ASCII PGM file\n", fileName);
+    setColor(RESET);
+    fclose(picture);
+    return NULL;
+  }
+  skipComments(picture);
+  fscanf(picture, "%d %d", width, height);
+  // get maximum value
+  skipComments(picture);
+  fscanf(picture, "%d", &numberHolder);
+  fgetc(picture);
+  pixelValues = (uint8_t*) malloc((*height) * (*width) * sizeof(uint8_t));
+  if (!pixelValues) {
+    setColor(RED);
+    printf("%s:%d > Failed to allocate memory\n", __FILE__, __LINE__);
+    setColor(RESET);
+    return NULL;
+  }
+  while (fscanf(picture, " %d ", &numberHolder) == 1) {
+    if (numberHolder > 255 || numberHolder < 0) {
+      setColor(RED);
+      printf("Error: corrupt input. Some pixels are not in [0-255]\n");
+      setColor(RESET);
+      fclose(picture);
+      return NULL;
+    }
+    pixelValues[i++] = numberHolder;
+  };
+  if (i != (*height) * (*width)) {
+    setColor(RED);
+    printf("Error: corrupt input. Read %ld pixels, expected %d.\n", i, (*height) * (*width));
+    setColor(RESET);
+    fclose(picture);
+    return NULL;
+  }
+  fclose(picture);
+  if (kernelSize) {
+    return addStaticPad(pixelValues, 0, *width, *height, kernelSize);
+  } else {
+    return pixelValues;
+  }
 }
 
 uint8_t* allocArrWithPad(int width, int height, int kernelSize) {
   // divide by 2 (floor)
   int padding = kernelSize>>1;
-  printf("allocating %d bytes\n", (width+padding*2)*(height+padding*2));
   return (uint8_t*) malloc((width+padding*2)*(height+padding*2));
+}
+
+/*
+* Function: addStaticPad
+* --------------------------
+* returns a pointer to an array with border padding set to paddingVal
+* and frees the pointer specified by pixelValues
+*
+* pixelValues: pointer to the array that will be padded
+* paddingVal: the value that padded pixels will be set to
+* width: image width without padding
+* height: image height without padding
+* kernelSize: the length of kernel dimension
+*
+* returns: a pointer to the newly allocated array with padding
+*/
+uint8_t* addStaticPad(uint8_t* pixelValues, uint8_t paddingVal, int width, int height, int kernelSize) {
+  size_t i, j;
+  uint8_t* arrValues;
+  int padding = kernelSize>>1;
+  // rowWidth is the width plus the padding on both sides
+  int rowWidth = width + padding*2;
+  arrValues = allocArrWithPad(width, height, kernelSize);
+  if (!arrValues) {
+    setColor(RED);
+    printf("%s:%d > Failed to allocate memory\n", __FILE__, __LINE__);
+    setColor(RESET);
+    return NULL;
+  }
+  //initialize padding rows with paddingVal
+  for (j=0, i=0; j < padding; i++, j++)
+    memset(arrValues + i*rowWidth, paddingVal, rowWidth);
+  //set pixelvalues adding padding at the beginning and end of each row
+  for (j=0; j < height; i++, j++) {
+    memset(arrValues + i*rowWidth, paddingVal, padding);
+    memcpy(arrValues + i*rowWidth+ padding, pixelValues + j*width, width * sizeof(uint8_t));
+    memset(arrValues + i*rowWidth + padding + width, paddingVal, padding);
+  }
+  //initialize last padding rows with paddingVal
+  for (j=0; j < padding; i++,j++)
+    memset(arrValues + i*rowWidth, paddingVal, rowWidth);
+  free(pixelValues);
+  return arrValues;
 }
 
 void removeExtension(char* filename, char* stripped) {
@@ -134,6 +284,9 @@ int* applyKernelArr(int* kernel, int kernelSize, float coefficient, uint8_t* pix
   int* outputArr;
   int padding, areaWidth, areaHeight;
   size_t i, j;
+  int (*applyKernelPix)(int*, int, float, uint8_t*, int, size_t, size_t) = applyKernelPixWithCo;
+  if (coefficient == 1)
+    applyKernelPix = applyKernelPixNoCo;
   padding = kernelSize >> 1;
   areaWidth = width - padding*2;
   areaHeight = height - padding*2;
@@ -145,7 +298,7 @@ int* applyKernelArr(int* kernel, int kernelSize, float coefficient, uint8_t* pix
   return outputArr;
 }
 
-int applyKernelPix(int* kernel, int kernelSize, float coefficient, uint8_t* pixelValues, int width, size_t xLoc, size_t yLoc) {
+int applyKernelPixWithCo(int* kernel, int kernelSize, float coefficient, uint8_t* pixelValues, int width, size_t xLoc, size_t yLoc) {
   size_t i, j;
   int result = 0, kernelPadding, row;
   kernelPadding = kernelSize >> 1;
@@ -158,7 +311,31 @@ int applyKernelPix(int* kernel, int kernelSize, float coefficient, uint8_t* pixe
   return (int)((float)(result) * coefficient);
 }
 
-uint8_t* filterScale(int* arrValues, int width, int height) {
+int applyKernelPixNoCo(int* kernel, int kernelSize, float coefficient, uint8_t* pixelValues, int width, size_t xLoc, size_t yLoc) {
+  size_t i, j;
+  int result = 0, kernelPadding, row;
+  kernelPadding = kernelSize >> 1;
+  for (i = 0; i < kernelSize; i++) {
+    row = xLoc+i-kernelPadding;
+    for (j = 0; j < kernelSize; j++){
+      result += pixelValues[(yLoc+j-kernelPadding)+row*width] * kernel[i*kernelSize+j];
+    }
+  }
+  return result;
+}
+
+/*
+* Function: filterSlice
+* --------------------------
+* sets all values grater than 255 to 255 and all values less than 0 to 0
+*
+* arrValues: a pointer the array that should be filtered
+* width: array width
+* height: array height
+*
+* returns: a pointer to the allocated array with filtered values
+*/
+uint8_t* filterSlice(int* arrValues, int width, int height) {
   size_t i;
   uint8_t* outputPixValues = (uint8_t*) malloc(width * height * sizeof(uint8_t));
   for (i = 0; i < height*width; i++) {
@@ -167,6 +344,17 @@ uint8_t* filterScale(int* arrValues, int width, int height) {
   return outputPixValues;
 }
 
+/*
+* Function: filterMinMax
+* --------------------------
+* normalizes arrValues values between [0-255]
+*
+* arrValues: a pointer the array that should be normalized
+* width: array width
+* height: array height
+*
+* returns: a pointer to the allocated array with normalized values
+*/
 uint8_t* filterMinMax(int* arrValues, int width, int height) {
   size_t i;
   int targetMin = 0, targetMax = 255, srcMin, srcMax, srcScale, targetScale, src, scaled;
@@ -179,17 +367,19 @@ uint8_t* filterMinMax(int* arrValues, int width, int height) {
     if (arrValues[i] > srcMax)
       srcMax = arrValues[i];
   }
-  printf("MIN:%d MAX:%d\n", srcMin, srcMax);
   if (srcMax == srcMin) {
-    if (srcMin < 255 && srcMin > 0) {
-      for (i = 0; i < width*height; i++) {
+    if (srcMin <= 255 && srcMin >= 0) {
+      for (i = 0; i < width*height; i++)
         outputPixValues[i] = arrValues[i];
-      }
-    } else if (srcMin > 255) {
+    } else if (srcMin >= 255) {
       memset(outputPixValues, 255, width*height);
     } else {
       memset(outputPixValues, 0, width*height);
     }
+    return outputPixValues;
+  } else if (srcMin <= 255 && srcMax >= 0) {
+    for (i = 0; i < width*height; i++)
+      outputPixValues[i] = arrValues[i];
     return outputPixValues;
   }
   srcScale = srcMax - srcMin;
@@ -203,9 +393,74 @@ uint8_t* filterMinMax(int* arrValues, int width, int height) {
   return outputPixValues;
 }
 
+/*
+* Function: setPaddingMirror
+* --------------------------
+* modifies arr by setting the values of the added padding pixels
+* to the nearest neighbor values
+*
+* arr: pixel values array with padding allocated
+* width: image width without padding
+* height: image height without padding
+* kernelSize: the length of kernel dimension
+*
+* returns: a bool value indicating failure as false and success as true
+*/
+void setPaddingMirror(uint8_t* arr, int width, int height, int kernelSize) {
+  size_t i, j, currPixLoc, rowWidth;
+  int padding = kernelSize >> 1;
+  rowWidth = padding*2+width;
+  // set top-left padding
+  currPixLoc = (padding*2+width)*padding+padding;
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < padding; j++)
+      arr[i*(padding*2+width)+j] = arr[currPixLoc];
+  }
+  // set top-right padding
+  currPixLoc = (padding*2+width)*padding+padding+width-1;
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < padding; j++)
+      arr[i*rowWidth+j+padding+width] = arr[currPixLoc];
+  }
+  // set bottom-left padding
+  currPixLoc = (padding*2+width)*(padding+height-1)+padding;
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < padding; j++)
+      arr[(i+height+padding)*rowWidth+j] = arr[currPixLoc];
+  }
+  // set bottom-right padding
+  currPixLoc = (padding*2+width)*(padding+height-1)+padding+width-1;
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < padding; j++)
+      arr[(i+padding+height)*rowWidth+width+padding+j] = arr[currPixLoc];
+  }
+  // set top border
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < width; j++)
+      arr[padding + i*rowWidth +j] = arr[padding + rowWidth*padding + j];
+  }
+  //set bottom border
+  for (i = 0; i < padding; i++) {
+    for (j = 0; j < width; j++)
+      arr[padding + (padding+height+i)*rowWidth +j] = arr[padding + rowWidth*(padding+height-1) + j];
+  }
+  // set left border
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < padding; j++)
+      arr[rowWidth*(i+padding)+j] = arr[rowWidth*(i+padding)+padding];
+  }
+  // set right border
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < padding; j++)
+      arr[rowWidth*(i+padding)+j+padding+width] = arr[rowWidth*(i+padding)+padding+width-1];
+  }
+}
+
 bool isBinaryPgm(FILE* file) {
   char* typeHolder = (char*) malloc(LINE_SIZE * sizeof(char));
-  fscanf(file, " %s ", typeHolder);
+  fread(typeHolder, sizeof(char), MAGIC_NUMBER_SIZE, file);
+  typeHolder[MAGIC_NUMBER_SIZE] = '\0';
+  strtok(typeHolder, "\n");
   if (strcmp(typeHolder, "P5")) {
     free(typeHolder);
     return false;
@@ -216,7 +471,9 @@ bool isBinaryPgm(FILE* file) {
 
 bool isAsciiPgm(FILE* file) {
   char* typeHolder = (char*) malloc(LINE_SIZE * sizeof(char));
-  fscanf(file, " %s ", typeHolder);
+  fread(typeHolder, sizeof(char), MAGIC_NUMBER_SIZE, file);
+  typeHolder[MAGIC_NUMBER_SIZE] = '\0';
+  strtok(typeHolder, "\n");
   if (strcmp(typeHolder, "P2")) {
     free(typeHolder);
     return false;
@@ -272,6 +529,37 @@ void skipComments(FILE* file) {
     fseek(file, -1, SEEK_CUR);
   }
   free(lineHolder);
+}
+
+/*
+* Function: staticToDynamic
+* --------------------------
+* returns a pointer to a user allocated array containing the same values of arr
+*
+* arr: pointer to the array that will be copied
+* kernelSize: the length of kernel dimension
+*
+* returns: a pointer to the allocated array with copied values
+*/
+int* staticToDynamicKernel(int arr[], int kernelSize) {
+  int* arrVal = malloc(kernelSize * kernelSize * sizeof(int));
+  memcpy(arrVal, arr, kernelSize * kernelSize * sizeof(int));
+  return arrVal;
+}
+
+/*
+* Function: doesKernelFit
+* --------------------------
+* checks if the kernel size fits the image without padding
+*
+* width: image width without padding
+* height: image height without padding
+* kernelSize: the length of kernel dimension
+*
+* returns: a bool indicating if the kernel can be applied before padding
+*/
+bool doesKernelFit(int kernelSize, int width, int height) {
+  return width > kernelSize && height > kernelSize;
 }
 
 //functions that are defined differently based on platform
